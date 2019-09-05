76fd6f87d81f0b66c546d54542d8bdc306afa947
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-04 22:11:21 -0300

    Exercício 8 - Exercício 7 (Revisado).

---
+++ dist/exercicio8.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var exercicio7_1 = require("./exercicio7");
+var total = new exercicio7_1.Piscina(6, 75);
+{
+    console.log(total.custoTotal());
+}
---
+++ exercicio8.ts
@@ -0,0 +1,6 @@
+import { Piscina } from "./exercicio7";
+
+let total = new Piscina(6, 75)
+{
+    console.log(total.custoTotal());
+}
