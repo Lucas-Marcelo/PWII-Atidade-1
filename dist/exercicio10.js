ae72572b585b1f8fcd588538480076962cafbaa7
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-04 22:32:27 -0300

    Exercício 10.

---
+++ dist/exercicio10.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var exercico9_1 = require("./exercico9");
+var n1 = 10;
+console.log("Cubo: " + exercico9_1.cubo(n1));
---
+++ exercicio10.ts
@@ -0,0 +1,4 @@
+import {cubo} from "./exercico9"
+
+let n1:number = 10;
+console.log(`Cubo: ${cubo(n1)}`);
