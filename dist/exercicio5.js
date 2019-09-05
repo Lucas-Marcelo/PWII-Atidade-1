4db94f573522f39d6b3b6f7c864791ceff8e5984
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-04 21:34:02 -0300

    Exercício 5.

---
+++ dist/exercicio5.js
@@ -0,0 +1,4 @@
+var largura = 10;
+var comprimento = 10;
+var area = function (larg, comp) { return (larg * comp); };
+console.log("\u00C1rea: " + area(largura, comprimento));
---
+++ exercicio5.ts
@@ -0,0 +1,6 @@
+let largura:number = 10;
+let comprimento:number = 10;
+
+let area = (larg:number, comp:number) => (larg * comp)
+
+console.log(`Área: ${area(largura, comprimento)}`);
