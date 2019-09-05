801e3f87ca6af78241bc5485dd96677977cbe064
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-04 21:38:59 -0300

    Exercício 6.

---
+++ dist/exercicio6.js
@@ -0,0 +1,3 @@
+var n1 = 3;
+var cubo = function (n) { return (n * n * n); };
+console.log("Cubo: " + cubo(n1));
---
+++ exercicio6.ts
@@ -0,0 +1,5 @@
+let n1:number = 3;
+
+let cubo = (n:number) => (n * n * n)
+
+console.log(`Cubo: ${cubo(n1)}`);
