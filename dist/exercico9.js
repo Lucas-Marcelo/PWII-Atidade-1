39a3421319047f8e224d27641f34a0d0a85739f8
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-09-04 22:25:23 -0300

    Exercício 9.

---
+++ dist/exercico9.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+//Função Exercício 4
+exports.fParImpar = function (numero) {
+    return numero % 2 == 0;
+};
+//Função Exercício 5
+exports.area = function (larg, comp) { return (larg * comp); };
+//Função Exercício 6
+exports.cubo = function (n) { return (n * n * n); };
---
+++ exercico9.ts
@@ -0,0 +1,12 @@
+//Função Exercício 4
+export let fParImpar = function(numero:number):boolean
+{
+    return numero % 2 == 0
+}
+
+//Função Exercício 5
+export let area = (larg:number, comp:number) => (larg * comp)
+
+
+//Função Exercício 6
+export let cubo = (n:number) => (n * n * n)
